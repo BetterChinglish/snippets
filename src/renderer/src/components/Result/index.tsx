b8bad0f7921cc20b8cf55e1636187c06a17155be
BetterChinglish <2199840669@qq.com>
2024-08-10 02:39:57 +0800

    增加小提示，显示完整的文本

--- src/renderer/src/components/Result/index.tsx
+++ src/renderer/src/components/Result/index.tsx
@@ -64,11 +64,20 @@ export default function Result(): React.JSX.Element {
   return (
     <>
       <main className="bg-slate-50 pl-6 pr-6 overflow-ellipsis whitespace-nowrap">
-        {dataList.map((item, index) => (
-          <div key={item.id} className={classNames("overflow-ellipsis overflow-hidden mb-3", {'choosed': index === selectedIndex})}>
-            {index + 1} : {item.content}
-          </div>
-        ))}
+        {dataList.map((item, index) => {
+          const isSelected = selectedIndex === index;
+          return (<div>
+            <div
+              key={item.id}
+              className={classNames("overflow-ellipsis overflow-hidden mb-3", { 'choosed': isSelected})}
+            >
+              {index + 1} : {item.content}
+            </div>
+            <div className={classNames('text-slate-600 w-full', {'selected-content': isSelected, 'no-selected-hidden': !isSelected})}>
+              {item.content}
+            </div>
+          </div>)
+        })}
       </main>
     </>
   )
